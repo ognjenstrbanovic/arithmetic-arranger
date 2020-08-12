d83de5102019308593001522ffee5b00caed4b0e
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-08-12 18:14:24 -0400

    unsure if solution but committing anyway to save progress and revert if needed

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -9,9 +9,14 @@ def arithmetic_arranger(problems):
         if len(problem.replace(" ", "").split("+")[0]) > 4 or len(problem.replace(" ", "").split("-")[0]) > 4 or len(problem.replace(" ", "").split("+")[1]) > 4 or len(problem.replace(" ", "").split("-")[1]) > 4:
             return "Error: Numbers cannot be more than four digits."
     else:
-        
-        if len(problem.replace(" ", "").split("+")[0]) > 2 or len(problem.replace(" ", "").split("-")[0]) > 2 or len(problem.replace(" ", "").split("+")[1]) > 2 or len(problem.replace(" ", "").split("-")[1]) > 2:
-            print("-----")
-        else:
-            print("----")
-            return arranged_problems
+        def arranged_problems():
+            for problem in problems:
+                if len(problem.replace(" ", "").split("+")[0]) > 2 or len(problem.replace(" ", "").split("-")[0]) > 2 or len(problem.replace(" ", "").split("+")[1]) > 2 or len(problem.replace(" ", "").split("-")[1]) > 2:
+                    print("-----")
+                else:
+                    print("----")
+                if "+" in problem:
+                    print(" " + " " + str(int(problem.replace(" ", "").split("+")[0] + int(problem.replace(" ", "").split("+")[1])))
+                else:
+                    print(" " + " " + str(int(problem.replace(" ", "").split("-")[0] - int(problem.replace(" ", "").split("-")[1])))
+            return arranged_problems()
