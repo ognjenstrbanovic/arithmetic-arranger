d2b040f451d69999595706189435572e955a4110
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-08-13 09:57:55 -0400

    incorrect output printed, but no errors; still work to be done

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -13,43 +13,52 @@ def arithmetic_arranger(problems):
         elif len(number_1) > 4 or len(number_2) > 4:
             return "Error: Numbers cannot be more than four digits."
         else:
+
             # no user error...
-            def arranged_problems():
-                # FIRST number of length ONE, printing number...
-                if len(number_1) == 1:
-                    print(" " + " " + " " + number_1 + "\n")
-                # FIRST number of length TWO, printing number...
-                elif len(number_1) == 2:
-                    print(" " + " " + number_1 + "\n")
-                # FIRST number of length THREE, printing number...
-                elif len(number_1) == 3:
-                    print(" " + number_1 + "\n")
-                # SECOND number of length ONE, printing number...
-                if len(number_2) == 1:
-                    print(" " + " " + " " + number_2 + "\n")
-                # SECOND number of length TWO, printing number...
-                elif len(number_2) == 2:
-                    print(" " + " " + number_2 + "\n")
-                # SECOND number of length THREE, printing number...
-                elif len(number_2) == 3:
-                    print(" " + number_2 + "\n")
-                # printing the LINE...
-                if len(number_1) > 3 or len(number_2) > 3:
-                    print("\n------\n")
-                elif len(number_1) > 3 or len(number_2) > 2:
-                    print("\n-----\n")
-                else:
-                    print("\n----\n")
+            # FIRST number of length ONE, printing number...
+            if len(number_1) == 1:
+                print(" " + " " + " " + number_1 + "\n")
+            # FIRST number of length TWO, printing number...
+            elif len(number_1) == 2:
+                print(" " + " " + number_1 + "\n")
+            # FIRST number of length THREE or FOUR, printing number...
+            else:
+                print(" " + number_1 + "\n")
 
-                # printing the SUM...
-                if "+" in problem:
-                    print("+" + " " + " " + str(int(number_1) + int(number_2)))
-                elif ("-" in problem) and (int(number_1) - int(number_2) > 0):
-                    print("-" + " " + " " + str(int(number_1) - int(number_2)))
-                else:
-                    print("-" + " " + str(int(number_1) - int(number_2)))
-                # print FOUR SPACES...
-                if problem != problems[-1]:
-                    print(" " + " " + " " + " ")
+            # SECOND number of length ONE, printing number...
+            if "+" in problem and len(number_2) == 1:
+                print("+" + " " + " " + " " + number_2 + "\n")
+            elif "-" in problem and len(number_2) == 1:
+                print("-" + " " + " " + " " + number_2 + "\n")
+            # SECOND number of length TWO, printing number...
+            elif "+" in problem and len(number_2) == 2:
+                print("+" + " " + " " + number_2 + "\n")
+            elif "-" in problem and len(number_2) == 2:
+                print("-" + " " + " " + number_2 + "\n")
+            # SECOND number of length THREE or FOUR, printing number...
+            elif "+" in problem and len(number_2) > 2:
+                print("+" + " " + number_2 + "\n")
+            else:
+                print("-" + " " + number_2 + "\n")
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+            # printing the LINE...
+            if len(number_1) > 3 or len(number_2) > 3:
+                print("------")
+            elif len(number_1) > 3 or len(number_2) > 2:
+                print("-----")
+            else:
+                print("----")
+
+            # printing the SUM...
+            if "+" in problem:
+                print(" " + " " + str(int(number_1) + int(number_2)))
+            elif ("-" in problem) and (int(number_1) - int(number_2) > 0):
+                print(" " + " " + str(int(number_1) - int(number_2)))
+            else:
+                print(" " + str(int(number_1) - int(number_2)))
+
+            # print FOUR SPACES...
+            if problem != problems[-1]:
+                print(" " + " " + " " + " ")
+
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
