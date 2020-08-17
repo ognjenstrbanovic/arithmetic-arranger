a0e60b284a5121ed4c2c637a5482d0c9ffe9cfd5
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-08-16 22:05:03 -0400

    very closegit add .! :-)

--- scripts/arithmetic_arranger.py
+++ scripts/arithmetic_arranger.py
@@ -18,12 +18,7 @@ def arithmetic_arranger(problems, true_or_false = False):
             # Line 1...
             arranged_problems[0].append(number_1)
             # Line 2...
-            if "+" in problem:
-                arranged_problems[1].append("+")
-                arranged_problems[1].append(number_2)
-            elif "-" in problem:
-                arranged_problems[1].append("-")
-                arranged_problems[1].append(number_2)
+            arranged_problems[1].append(number_2)
             # Line 3
             if (max(len(number_1), len(number_2))) == 1:
                 arranged_problems[2].append("---")
@@ -48,80 +43,36 @@ def arithmetic_arranger(problems, true_or_false = False):
     line_two = arranged_problems[1]
     line_three = arranged_problems[2]
     line_four = arranged_problems[3]
-    print(arranged_problems)
+    problem_width = []
+    for element in line_three:
+        problem_width.append(len(element))
     # Output formatting...
-    for element in line_one:
-        if element != line_one[-1]:
-            if len(number_1) > (len(number_2 + " " + " ")):
-                print(element, end = "    ")
-            else:
-                print(element.rjust(len(number_2) - len(number_1)), end = "    ")
+    for i in range(len(problems)):
+        if line_one[i] != line_one[-1]:
+            print(line_one[i].rjust(problem_width[i]), end = "    ")
         else:
-            print(element)
-    for element in line_two:
-        # if element != line_two[-1]:
-            
-        # else:
-
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)
-
-#             if max(len(number_1), len(number_2)) < 5:
-#                 line_two.append(number_2)
-#     for string in arranged_problems:
-#         if element != line_one[-1]:
-#             print(element.rjust(2), end = "    ")
-#         else:
-#             print(element.rjust(2))
-#     for element in line_two:
-#         if element != line_two[-1]:
-#             print("+".ljust(0), end = "")
-#             print(element.rjust(2), end = "    ")
-# arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)
-
-#             if "+" in problem:
-#                 "+".ljust(1)
-#                 if max(len(number_1), len(number_2)) < 2:
-#                     line_two.append("{:=+d}".format(int(number_2)))
-#             else:
-#                 "-".ljust(1)
-#                 line_two.append("{:= d}".format(int(number_2)))
-#             # Line 3...
-#             if len(problem) < 7:
-#                 line_three.append('{:^16}'.format("---"))
-#             elif len(problem) < 8:
-#                 line_three.append("----")
-#             elif len(problem) < 9:
-#                 line_three.append("-----")
-#             elif len(problem) < 11:
-#                 line_three.append("------")
-#             else:
-#                 line_three.append("-------")
-#             # Line 4...
-#             if true_or_false == True:
-#                 if "+" in problem:
-#                     line_four.append(str(int(number_1) + int(number_2)))
-#                 elif "-" in problem:
-#                     if int(number_1) - int(number_2) > 0:
-#                         line_four.append(str(int(number_1) - int(number_2)))
-#                     elif int(number_1) - int(number_2) < 0:
-#                         line_four.append(str(int(number_1) - int(number_2)))
-#     for element in line_one:
-#         if element != line_one[-1]:
-#             print(f"{element:>5}", end = "    ")
-#         else:
-#             print(f"{element:>5}")
-#     for element in line_two:
-#         if element != line_two[-1]:
-#             print(f"{element:>5}", end = "    ")
-#         else:
-#             print(f"{element:>5}")
-#     for element in line_three:
-#         if element != line_three[-1]:
-#             print(f"{element:>5}", end = "    ")
-#         elif element == line_three[-1]:
-#             print(f"{element:>5}")
-#     for element in line_four:
-#         if element != line_four[-1]:
-#             print(f"{element:>5}", end = "    ")
-#         else:
-#             print(f"{element:>5}")
+            print(line_one[i].rjust(problem_width[i]))
+    for i in range(len(problems)):
+        if "+" in problem:
+            if line_two[i] != line_two[-1]:
+                print("+" + line_two[i].rjust(problem_width[i] - 1), end = "    ")
+            else:
+                print("+" + line_two[i].rjust(problem_width[i] - 1))
+        if "-" in problem:
+            if line_two[i] != line_two[-1]:
+                print("-" + line_two[i].rjust(problem_width[i] - 1), end = "    ")
+            else:
+                print("-" + line_two[i].rjust(problem_width[i] - 1))
+    print("    ".join(line_three))
+    for i in range(len(problems)):
+        if "-" in problem and number_2 > number_1:
+            if line_four[i] != line_four[-1]:
+                print("-" + line_four[i].rjust(problem_width[i] - 1), end = "    ")
+            else:
+                print("-" + line_four[i].rjust(problem_width[i] - 1))
+        else:
+            if line_four[i] != line_four[-1]:
+                print(line_four[i].rjust(problem_width[i]), end = "    ")
+            else:
+                print(line_four[i].rjust(problem_width[i]))
+arithmetic_arranger(["32 - 698", "3801 - 2", "45 + 43", "123 + 49"], True)
